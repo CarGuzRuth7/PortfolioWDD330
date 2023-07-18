3a57e2390948e767230596e2d0680a02917ad780
CarGuzRuth7 <cardonaguzmanruth97@gmail.com>
2023-07-18 19:24:52 -0300

    Fixed delete pokemon

--- scripts/w9.js
+++ scripts/w9.js
@@ -3,41 +3,45 @@ function getFavList(){
     const favList = document.querySelector(".fav-list");
     let favPokeList =  JSON.parse(localStorage.getItem("fav-list")) || [];
     favPokeList.forEach((item)=>{
-    let img = document.createElement("img");
+    let div = document.createElement("div")
+    let img = document.createElement("img");   
     let deleteBtn = document.createElement("button");
+    div.style.width = "96px";
+    div.style.display= "inline-block";
+    div.style.position = "relative"
     deleteBtn.innerHTML = "X";
-    deleteBtn.forEach((del)=>{
-        del.style.position = "absolute";
-        del.style.right = "0";
-        del.style.fontSize = "25px";
-        del.style.cursor = "pointer";
-        del.style.border = "none";
-        del.style.backgroundColor = "transparent";
-        del.addEventListener("mouseover", ()=>{
-            del.style.color = "red";
-
-        })
-        del.addEventListener("mouseout", ()=>{
-            del.style.color = "black";
-
-        })
-    });
-    del.addEventListener("click",()=>{
-        let idPokedex = item.id
-        const index = favList.findIndex((poke) => poke.id === idPokedex);
-        if (index !== -1) { //get specific index and from that specific list
-            favList.splice(index, 1);
-            window.localStorage.setItem("fav-list", JSON.stringify(favList));
-           setTimeout(function(){
-            window.location.reload();
-         }, 1000);
-         }
-    });
-
+    deleteBtn.style.position = "absolute";
+    deleteBtn.style.right = "0";
+    deleteBtn.style.fontSize = "25px";
+    deleteBtn.style.cursor = "pointer";
+    deleteBtn.style.border = "none";
+    deleteBtn.style.backgroundColor = "transparent";
+    deleteBtn.classList.add("delete-pokemon")
+    deleteBtn.setAttribute("data-id", item.id)
     img.setAttribute("src", `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${item.id}.png`);
-    favList.appendChild(deleteBtn)
-    favList.appendChild(img);
+    img.setAttribute("alt", "pokemon image");
+    div.appendChild(deleteBtn)
+    div.appendChild(img);
+    favList.appendChild(div)
     
 });
 }
 getFavList()
+const deleteBtn = document.querySelectorAll(".delete-pokemon"); 
+    deleteBtn.forEach((del)=>{
+        del.addEventListener("mouseover", ()=>{del.style.color = "red";});
+        del.addEventListener("mouseout", ()=>{del.style.color = "black";});
+        del.addEventListener("click",()=>{
+            let idPokedex = del.getAttribute("data-id");
+            let favList = JSON.parse(localStorage.getItem("fav-list")) || [];
+            const index = favList.findIndex((poke) => poke.id === idPokedex);
+            if (index !== -1) { //get specific index and from that specific list
+                favList.splice(index, 1);
+                window.localStorage.setItem("fav-list", JSON.stringify(favList));
+               setTimeout(function(){
+                window.location.reload();
+             }, 1000);
+             }
+        });
+    });
+    
